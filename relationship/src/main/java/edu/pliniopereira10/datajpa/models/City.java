c0becdd0cbfc3ca7759c3497e84cfadc429ba559
pliniopereira10 <plinio.pereira10@gmail.com>
2023-01-05 09:27:15 -0300

    If applied, this commit will create the entities classes.

---
+++ relationship/src/main/java/edu/pliniopereira10/datajpa/models/City.java
@@ -0,0 +1,27 @@
+package edu.pliniopereira10.datajpa.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_city")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+}
