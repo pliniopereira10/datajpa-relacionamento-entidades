2560f1220c58903a4c7756a450652b941ac475ac
pliniopereira10 <plinio.pereira10@gmail.com>
2023-01-05 10:07:41 -0300

    If applied, this commit will create a one-way @ManyToOne
    associative relationship.

--- relationship/src/main/java/edu/pliniopereira10/datajpa/models/Author.java
+++ relationship/src/main/java/edu/pliniopereira10/datajpa/models/Author.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,9 +21,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Author implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "zipcode_id")
+	private Zipcode zipcode;
 }
